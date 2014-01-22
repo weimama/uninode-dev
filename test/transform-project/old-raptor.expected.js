3801e85e278e4a7c461e9c539f5658b0908941d9
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-01-22 10:54:22 -0700

    Minor fix for require('raptor').extend

--- test/transform-project/old-raptor.expected.js
+++ test/transform-project/old-raptor.expected.js
@@ -1,5 +1,5 @@
 var forEachEntry = require('raptor-util').forEachEntry;
 var createError = require('raptor-util').createError;
-var extend = require('raptor').extend;
+var extend = require('raptor-util').extend;
 forEachEntry({});
 createError(new Error('test'));

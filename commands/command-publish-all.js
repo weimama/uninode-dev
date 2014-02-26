961f784ffd43b4b5aba5488566489758721a946c
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-26 14:51:57 -0700

    Added helper "publish all" command

---
+++ commands/command-publish-all.js
@@ -0,0 +1,85 @@
+'use strict';
+
+require('raptor-ecma/es6');
+var File = require('raptor-files/File');
+var raptorPromises = require('raptor-promises');
+
+var nodePath = require('path');
+
+module.exports = {
+    usage: 'Usage: $0 $commandName [dir]',
+
+    options: {
+    },
+
+    validate: function(args, rapido) {
+        var dir = args._[0];
+        if (dir) {
+            dir = nodePath.resolve(process.cwd(), dir);
+        }
+        else {
+            dir = process.cwd();
+        }
+        
+        return {
+            dir: dir
+        };
+    },
+
+    run: function(args, config, rapido) {
+        var dir = args.dir;
+
+        dir = new File(dir);
+
+        var children = dir.listFiles();
+
+        var modulesToPublish = [];
+        var failedModules = {};
+        var failed = false;
+
+        for (var i=0; i<children.length; i++) {
+            var childDir = children[i];
+            if (childDir.getName().startsWith('raptor-') || childDir.getName() === 'rapido') {
+                var gitDir = new File(childDir, '.git');
+                if (gitDir.exists()) {
+                    modulesToPublish.push(childDir.getName());
+                }
+            }
+        }
+
+        modulesToPublish.sort();
+
+        modulesToPublish = ['raptor-util'];
+
+        console.log('Publishing the following modules:\n- ' + modulesToPublish.join('\n- '));
+
+        var promises = modulesToPublish.map(function(moduleName) {
+            var moduleDir = new File(dir, moduleName);
+            var promise = rapido.runCommand('module', 'publish', {
+                    cwd: moduleDir.getAbsolutePath()
+                });
+
+            promise.fail(function(e) {
+                failed = true;
+                failedModules[moduleName] = e;
+            });
+
+            return promise;
+        });
+
+        return raptorPromises.allSettled(promises)
+            .then(function() {
+
+                if (failed) {
+                    var message = Object.keys(failedModules).sort().map(function(moduleName) {
+                        return 'Module name: ' + moduleName + '\nReason: ' + failedModules[moduleName];
+                    }).join('\n\n');
+
+                    throw 'The following modules failed to publish:\n\n' + message;
+                } else {
+                    rapido.log();
+                    rapido.log.success('All modules successfully published!');    
+                }                
+            });
+    }
+};

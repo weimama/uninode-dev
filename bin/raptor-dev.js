c1f0b225647557dd2061cb697d2a5c0947af1e52
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-02-26 13:48:34 -0700

    Switched to simpler method for running rapido

--- bin/raptor-dev.js
+++ bin/raptor-dev.js
@@ -1,23 +1,18 @@
 'use strict';
 
-if( !process.env.NODE_ENV ) {
-    process.env.NODE_ENV = 'development';
-}
-
 var path = require('path');
-var fs = require('fs');
-var rapido = require('rapido').create();
 
-rapido.title = 'Commands for RaptorJS 3 development';
-rapido.configFilename = 'raptor-migrate.json';
-rapido.addStackDir(path.join(__dirname, '..'));
-rapido.enableStack('raptor-dev');
-
-Object.defineProperty(rapido, 'version', {
-    get: function() {
-        var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8'));
-        return pkg.version;
+require('rapido').run(process.argv, {
+    title: 'Commands for RaptorJS 3 development',
+    configFilename: 'raptor-dev.json',
+    stackDirs: [
+        path.join(__dirname, '..')
+    ],
+    enabledStacks: [
+        'raptor-dev',
+        'module'
+    ],
+    version: function() {
+        return require('../package.json').version;
     }
 });
-
-rapido.run(process.argv);

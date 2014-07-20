26883b60920ac844dcda6c68bff6caf4d874a79c
weima <weima@ebay.com>
2014-07-20 00:18:47 -0700

    fix defer

--- project/src/migrate/ep-ebay.js
+++ project/src/migrate/ep-ebay.js
@@ -12,7 +12,7 @@ module.exports.middleware.getQualifiedTreatments = function(options) {
 };
 
 module.exports.getEpContext = function(req) {
-    var defer = require('q').deferred();
+    var defer = require('q').defer();
     ebayEp.exp(req, null, function(err, exp) {
         if(err || !exp) {
             return defer.reject(err);
